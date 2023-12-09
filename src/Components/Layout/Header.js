3b5a101bfe5fa4b51fc864768a6154820601496c
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-12-10 01:02:16 +0530

    Added Restaurant Summary

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from './Header.module.css';
 import banner from './meals.jpg';
 import CartButton from './HeaderCartButton';
+import Summary from "./Summary";
 
 const Header = props => {
     
@@ -14,6 +15,7 @@ const Header = props => {
             <div className={classes['main-image']}>
                 <img src={banner} alt='Error Loading' />
             </div>
+            <Summary/>
             
         </React.Fragment>
     );
